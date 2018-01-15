5e81b2055419c5105530749a324515cf0fdb7306
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-01-15 20:52:58 +0300

    Modified hero-detail angular routes syntax

--- src/app/config/app-routing.module.ts
+++ src/app/config/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent }      from '../controllers/heroes/heroes.component';
 import { DashboardComponent }   from '../controllers/dashboard/dashboard.component';
+import { HeroDetailComponent }  from '../controllers/heroes/hero-detail/hero-detail.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
--- src/app/controllers/heroes/heroes.component.ts
+++ src/app/controllers/heroes/heroes.component.ts
@@ -12,7 +12,6 @@ import { HeroService } from '../../services/hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -23,10 +22,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
